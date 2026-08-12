b9c21aa5e0aeb087c309cfb6b6e72d4d2191dfc3
César <davicesar602@gmail.com>
2026-08-12 09:56:57 -0300

    esqueci de apagar na main

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import os
-import shutil
+import gerenciador_de_arquivos
 
 caminho_antigo = r"C:\Users\davic\Downloads\testedeleitura"
 caminho_novo = r"C:\Users\davic\Downloads\pastaparamover"
-
-try:
-    os.mkdir(caminho_novo)
-except FileExistsError as e:
-    print("O caminho que deseja criar ja existe")
-
-for root, dirs, files in os.walk(caminho_antigo):
-    for file in files:
-        old_file_path = os.path.join(root, file)
-        new_file_path = os.path.join(caminho_novo, file)
-        print(new_file_path)
-
-        shutil.move(old_file_path, new_file_path)
-        print(f"O arquivo {file} foi movido com sucesso!")
